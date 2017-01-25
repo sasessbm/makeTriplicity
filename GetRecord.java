3c905c0f89994aecdec37b5cacff4c46c416b90e
sasessbm <marth.falco.ssbm@gmail.com>
2017-01-25 17:02:02 +0900

    ３つのクラス作成

---
+++ GetRecord.java
@@ -0,0 +1,70 @@
+package makeTriplicity;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+public class GetRecord {
+
+	//プロキシ設定
+	private static Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("proxy.nagaokaut.ac.jp", 8080));
+
+	public static void main(String[] args) throws Exception {
+
+		Connection con = null;
+		int idCount = 0;
+		try {
+			// JDBCドライバのロード - JDBC4.0（JDK1.6）以降は不要
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			
+			// MySQLに接続
+			//con = DriverManager.getConnection("jdbc:mysql://localhost:3306/tobyo_db?useSSL=false", "root", "databasetest86");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb?useSSL=false", "root", "databasetest86");
+			System.out.println("MySQLに接続できました。");
+
+			Statement stm = con.createStatement();
+
+			String sql = "";
+			
+			//とりあえず最初の10個を取得
+			for(int id =1; id <= 10; id++){
+				
+				Record record;
+				
+				sql = "select snippet from testTable where id = " + id;
+				ResultSet rs = stm.executeQuery(sql);
+
+				while(rs.next()){
+					record = new Record(id, rs.getString("snippet"), rs.getString("medicineName"), rs.getString("diseaseName"), 
+										rs.getString("sex"),rs.getString("title_blog"),rs.getString("title_blogArticle"),
+										rs.getString("url_blogArticle"),rs.getString("age"),rs.getString("blogArticle") );
+				}
+			}
+
+
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+			System.out.println("JDBCドライバのロードに失敗しました。");
+		} catch (SQLException e) {
+			System.out.println("MySQLに接続できませんでした。" + e);
+		} finally {
+			System.out.println("test");
+			if (con != null) {
+				try {
+					con.close();
+					System.out.println("MySQLのクローズに成功しました。");
+				} catch (SQLException e) {
+					System.out.println("MySQLのクローズに失敗しました。");
+				}
+			}
+		}
+
+	}
+
+
+}
---
+++ Record.java
@@ -0,0 +1,81 @@
+package makeTriplicity;
+
+public class Record {
+	
+	private int id;
+	private String snippet;
+	private String medicineName;
+	private String diseaseName;
+	private String sex;
+	private String title_blog;
+	private String title_blogArticle;
+	private String url_blogArticle;
+	private String age;
+	private String blogArticle;
+	
+	//コンストラクタ
+	public Record(int id, String snippet, String medicineName,
+			String diseaseName, String sex, String title_blog,
+			String title_blogArticle, String url_blogArticle, String age,
+			String blogArticle) {
+		this.id = id;
+		this.snippet = snippet;
+		this.medicineName = medicineName;
+		this.diseaseName = diseaseName;
+		this.sex = sex;
+		this.title_blog = title_blog;
+		this.title_blogArticle = title_blogArticle;
+		this.url_blogArticle = url_blogArticle;
+		this.age = age;
+		this.blogArticle = blogArticle;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSnippet() {
+		return snippet;
+	}
+
+	public String getMedicineName() {
+		return medicineName;
+	}
+	
+	public void setMedicineName(String medicineName) {
+		this.medicineName = medicineName;
+	}
+
+	public String getDiseaseName() {
+		return diseaseName;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public String getTitle_blog() {
+		return title_blog;
+	}
+
+	public String getTitle_blogArticle() {
+		return title_blogArticle;
+	}
+
+	public String getUrl_blogArticle() {
+		return url_blogArticle;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public String getBlogArticle() {
+		return blogArticle;
+	}
+
+}
